d9b5c9c249ca522e8d89b2457763f438bafbab65
david <4504352+northernocean@users.noreply.github.com>
2020-07-09 01:38:50 +0000

    Add Camps Controller

---
+++ src/Controllers/CampsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CampsController : ControllerBase
+    {
+        public object Get()
+        {
+            return new { Moniker = "ATL2018", Name = "Atlanta Code Camp" };
+        }
+    }
+}
